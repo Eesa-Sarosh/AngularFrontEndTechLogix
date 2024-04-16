95ad1fa271d2b98b83478dec2eab4aa6b20c055d
eesasarosh <eesasarosh@gmail.com>
2024-04-16 15:24:44 +0500

    App Structure Created

--- payment-app/src/app/app.module.ts
+++ payment-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details/payment-details-form/payment-details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent
   ],
   imports: [
     BrowserModule,
